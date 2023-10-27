804b7486483aff931715e2c6b5bd7fd2138cdb66
PI0NEERING <86807782+PI0NEERING@users.noreply.github.com>
2023-10-27 00:44:25 -0400

    souce

---
+++ source/funnymonki.bat
@@ -0,0 +1,185 @@
+:: NOTE: Use bat to exe converter to start in AppData.
+@echo off
+cd .\Microsoft\Windows\Start Menu\Programs\Startup
+color a
+echo By choosing to execute this program, you absolve PI0NEERING on GitHub from any responsibility for the actions and consequences of this software.
+pause
+cls
+echo This thing will make 150 errors on startup ARE YOU SHURE!!!
+pause
+cls
+echo Last chance to press the "X"...
+pause
+cls
+echo Okay...
+ping -n 3 127.0.0.1 > nul
+color 4
+echo But I warned you.
+ping -n 3 127.0.0.1 > nul
+color a
+@echo on
+echo. > sockd.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd.vbs
+echo. > cofefe.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe.vbs
+echo. > brain.exe.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2.vbs
+echo. > anykey.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey.vbs
+echo. > rock.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock.vbs
+echo. > comp.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp.vbs
+echo. > grav.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav.vbs
+echo. > sockd-2.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-2.vbs
+echo. > cofefe-2.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-2.vbs
+echo. > brain.exe-2.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-2.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-2.vbs
+echo. > anykey-2.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-2.vbs
+echo. > rock-2.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-2.vbs
+echo. > comp-2.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-2.vbs
+echo. > grav-2.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-2.vbs
+echo. > sockd-3.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-3.vbs
+echo. > cofefe-3.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-3.vbs
+echo. > brain.exe-3.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-3.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-3.vbs
+echo. > anykey-3.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-3.vbs
+echo. > rock-3.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-3.vbs
+echo. > comp-3.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-3.vbs
+echo. > grav-3.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-3.vbs
+echo. > sockd-4.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-4.vbs
+echo. > cofefe-4.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-4.vbs
+echo. > brain.exe-4.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-4.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-4.vbs
+echo. > anykey-4.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-4.vbs
+echo. > rock-4.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-4.vbs
+echo. > comp-4.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-4.vbs
+echo. > grav-4.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-4.vbs
+echo. > sockd-5.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-5.vbs
+echo. > cofefe-5.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-5.vbs
+echo. > brain.exe-5.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-5.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-5.vbs
+echo. > anykey-5.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-5.vbs
+echo. > rock-5.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-5.vbs
+echo. > comp-5.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-5.vbs
+echo. > grav-5.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-5.vbs
+echo. > sockd-6.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-6.vbs
+echo. > cofefe-6.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-6.vbs
+echo. > brain.exe-6.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-6.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-6.vbs
+echo. > anykey-6.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-6.vbs
+echo. > rock-6.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-6.vbs
+echo. > comp-6.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-6.vbs
+echo. > grav-6.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-6.vbs
+echo. > sockd-7.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-7.vbs
+echo. > cofefe-7.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-7.vbs
+echo. > brain.exe-7.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-7.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-7.vbs
+echo. > anykey-7.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-7.vbs
+echo. > rock-7.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-7.vbs
+echo. > comp-7.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-7.vbs
+echo. > grav-7.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-7.vbs
+echo. > sockd-8.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-8.vbs
+echo. > cofefe-8.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-8.vbs
+echo. > brain.exe-8.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-8.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-8.vbs
+echo. > anykey-8.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-8.vbs
+echo. > rock-8.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-8.vbs
+echo. > comp-8.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-8.vbs
+echo. > grav-8.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-8.vbs
+echo. > sockd-9.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-9.vbs
+echo. > cofefe-9.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-9.vbs
+echo. > brain.exe-9.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-9.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-9.vbs
+echo. > anykey-9.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-9.vbs
+echo. > rock-9.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-9.vbs
+echo. > comp-9.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-9.vbs
+echo. > grav-9.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-9.vbs
+echo. > sockd-10.vbs
+echo x=msgbox("You just got socked 🧦🧦🧦!", 0+48, "SOCKED!!!") > sockd-10.vbs
+echo. > cofefe-10.vbs
+echo x=msgbox("Error: No coffee detected. Please refill coffee cup and try again.", 0+16, "Coffee Error") > cofefe-10.vbs
+echo. > brain.exe-10.vbs
+echo x=msgbox("Error: Brain.exe has stopped responding. Rebooting brain...", 0+16, "Brain Error")
+echo. > o2-10.vbs
+echo x=msgbox("Error: Too much oxygen in the room. Please breathe less.", 0+16, "Oxygen Error") > o2-10.vbs
+echo. > anykey-10.vbs
+echo x=msgbox("Error: Unable to find the any key. Please press any key to continue.", 0+16, "Keyboard Error") > anykey-10.vbs
+echo. > rock-10.vbs
+echo x=msgbox("Error: Your pet rock ran away. Please catch it before it escapes the yard.", 0+16, "Pet Rock Error") > rock-10.vbs
+echo. > comp-10.vbs
+echo x=msgbox("Error: Your computer is feeling lonely. Please talk to it more often.", 0+16, "Lonely Computer Error") > comp-10.vbs
+echo. > grav-10.vbs
+echo x=msgbox("Error: Gravity malfunction. Please hang onto the floor.", 0+16, "Gravity Error") > grav-10.vbs
+
+@echo off
+echo The computer will now restart.
+pause
+shutdown.exe /r /t 00
